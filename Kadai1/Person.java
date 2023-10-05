5637ddf29036086e44d9473d518205000a0b016d
Masashi <k7007674@gmail.com>
2023-10-05 11:06:21 +0900

    Simplify switch code in Person.java#1

--- Kadai1/Person.java
+++ Kadai1/Person.java
@@ -1,6 +1,14 @@
 public class Person{
     //enumで性別を限定
-    public enum Gender{MAN, WOMAN};
+    public enum Gender{
+        MAN("man"), WOMAN("woman");
+        //enumのフィールドの定義
+        private String speakGender;
+        //enumのコンストラクタの定義
+        private Gender(String speakGender){
+            this.speakGender = speakGender;
+        }
+    }
     private Gender gender;
     
     //インスタンスの生成時に性別を設定
@@ -10,15 +18,18 @@ public class Person{
 
     //インスタンスメソッド speak
     public void speak(){
-        switch(gender){
-            case MAN:
-                System.out.println("I'm a man");
-                break;
-            case WOMAN:
-                System.out.println("I'm a woman");
-                break;
-            default:
-        }
+        // switch(gender){
+        //     case MAN:
+        //         System.out.println("I'm a man");
+        //         break;
+        //     case WOMAN:
+        //         System.out.println("I'm a woman");
+        //         break;
+        //     default:
+        // }
+        //上記switch文を列挙型Genderのフィールドに変更
+        System.out.println("I'm a "+ gender.speakGender);
+
     }
 
     public static void main(String[] args){
